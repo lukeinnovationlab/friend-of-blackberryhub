5d93588edc229dedc1b150747a92b3ca73f31621
Luke Chen <lchen@blackberry.com>
2016-06-13 17:53:19 -0400

    Integration with Firebase Invite.

--- app/src/main/java/com/lukeinnovationlab/friendofblackberryhub/MainActivity.java
+++ app/src/main/java/com/lukeinnovationlab/friendofblackberryhub/MainActivity.java
@@ -1,27 +1,104 @@
 package com.lukeinnovationlab.friendofblackberryhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.common.ConnectionResult;
 import com.lukeinnovationlab.friendofblackberryhub.firebase.invite.InviteHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InviteHelper.InviteCallback {
 
     private static final String TAG = "MainActivity";
+
+    private TextView mViewPromotionCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPromotionCode = (TextView) findViewById(R.id.text_promotion_code);
+
+        InviteHelper.getInstance().checkInvitation(this, this, true);
     }
 
-    public void onShare(View view) {
-        Log.i(TAG, "onShare");
+    public void onInvite(View view) {
+        Log.i(TAG, "onInvite");
 
-        if (view.getId() == R.id.action_share) {
-            InviteHelper.getInstance().shareByInvite(this);
+        if (view.getId() == R.id.action_invite) {
+            InviteHelper.getInstance().invite(this);
+        }
+    }
+
+    public void onSharePromotion(View view) {
+        Log.i(TAG, "onSharePromotion");
+
+        if (view.getId() == R.id.action_share_promotion) {
+            InviteHelper.getInstance().sharePromotion(this);
+        }
+    }
+
+    /**
+     * This is called after the InviteHelper.invite() or InviteHelper.sharePromotion() activity
+     * completes.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.i(TAG, "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
+
+        if (requestCode == InviteHelper.REQUEST_INVITE_BLACKBERRY_HUB) {
+            if (resultCode == RESULT_OK) {
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Log.i(TAG, "onActivityResult: sent invitation " + id);
+                }
+
+                Log.i(TAG, "Invited");
+                Toast.makeText(this, R.string.result_invited_app, Toast.LENGTH_SHORT).show();
+            } else {
+                // Sending failed or it was canceled, show failure message to the user
+                Log.w(TAG, "Failed to invite: " + resultCode);
+                Toast.makeText(this, R.string.result_failed_to_invite_app, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } else if (requestCode == InviteHelper.REQUEST_SHARE_BLACKBERRY_HUB_AD_FREE) {
+            if (resultCode == RESULT_OK) {
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Log.i(TAG, "onActivityResult: sent promotion " + id);
+                }
+
+                Log.i(TAG, "Sent promotion");
+                Toast.makeText(this, R.string.result_send_promotion, Toast.LENGTH_SHORT).show();
+            } else {
+                // Sending failed or it was canceled, show failure message to the user
+                Log.w(TAG, "Failed to send promotion: " + resultCode);
+                Toast.makeText(this, R.string.result_failed_to_send_promotion, Toast
+                        .LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "Failed to invite: " + connectionResult);
+        Toast.makeText(this, R.string.result_failed_to_invite_app, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPromotionCodeReceived(String promotionCode) {
+        if (mViewPromotionCode != null) {
+            mViewPromotionCode.setText(getString(R.string.text_promotion_code_received) + promotionCode);
         }
     }
 }
--- app/src/main/java/com/lukeinnovationlab/friendofblackberryhub/firebase/invite/InviteHelper.java
+++ app/src/main/java/com/lukeinnovationlab/friendofblackberryhub/firebase/invite/InviteHelper.java
@@ -1,9 +1,19 @@
 package com.lukeinnovationlab.friendofblackberryhub.firebase.invite;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInvite;
+import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.appinvite.AppInviteInvitationResult;
+import com.google.android.gms.appinvite.AppInviteReferral;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.lukeinnovationlab.friendofblackberryhub.R;
 
 /**
@@ -14,7 +24,14 @@ public final class InviteHelper {
 
     private static final String TAG = "InviteHelper";
 
+    public static final int REQUEST_INVITE_BLACKBERRY_HUB = 1;
+    public static final int REQUEST_SHARE_BLACKBERRY_HUB_AD_FREE = 2;
+
     private static InviteHelper sHelper = null;
+
+    public interface InviteCallback extends GoogleApiClient.OnConnectionFailedListener {
+        void onPromotionCodeReceived(final String promotionCode);
+    }
 
     private InviteHelper() {
     }
@@ -30,8 +47,117 @@ public final class InviteHelper {
         return sHelper;
     }
 
-    public void shareByInvite(final Context context) {
-        Log.i(TAG, "Shared by Invite");
-        Toast.makeText(context, R.string.result_shared, Toast.LENGTH_SHORT).show();
+    public void invite(final Context context) throws RuntimeException {
+        Log.i(TAG, "To invite");
+
+        Intent intent = new AppInviteInvitation.IntentBuilder(context.getString(R.string
+                .invitation_title))
+                .setMessage(context.getString(R.string.invitation_message))
+                .setDeepLink(Uri.parse(context.getString(R.string
+                        .invitation_deep_link_blackberry_hub)))
+                .setEmailHtmlContent(context.getString(R.string.invitation_email_html_content))
+                .setEmailSubject(context.getString(R.string.invitation_email_subject))
+                // .setAccount(new Account("", GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE)) // Must be a
+                // Google account
+                .build();
+
+        if (context instanceof Activity) {
+            ((Activity) context).startActivityForResult(intent, REQUEST_INVITE_BLACKBERRY_HUB);
+        } else {
+            throw new RuntimeException("Failed to invite: invalid activity.");
+        }
+    }
+
+    public void sharePromotion(final Context context) {
+        String promotionCode = generatePromotionCode();
+
+        Log.i(TAG, "To share promotion code: " + promotionCode);
+
+        Intent intent = new AppInviteInvitation.IntentBuilder(context.getString(R.string
+                .promotion_title))
+                .setMessage(context.getString(R.string.promotion_message))
+                .setDeepLink(Uri.parse(context.getString(R.string
+                        .promotion_deep_link_promotion_code) + promotionCode))
+                .setEmailHtmlContent(context.getString(R.string.promotion_email_html_content))
+                .setEmailSubject(context.getString(R.string.promotion_email_subject))
+                // .setAccount(new Account("", GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE)) // Must be a
+                // Google account
+                .build();
+
+        if (context instanceof Activity) {
+            ((Activity) context).startActivityForResult(intent,
+                    REQUEST_SHARE_BLACKBERRY_HUB_AD_FREE);
+        } else {
+            throw new RuntimeException("Failed to share promotion: invalid activity.");
+        }
+    }
+
+    private String generatePromotionCode() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("AD_FREE_");
+
+        String time = String.valueOf(System.currentTimeMillis());
+        sb.append(time.substring(time.length() - 5));
+        return sb.toString();
+    }
+
+    public void checkInvitation(final Context context, final InviteCallback inviteCallback, boolean
+            autoLaunchDeepLink) {
+        // Create an auto-managed GoogleApiClient with access to App Invites.
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(AppInvite.API)
+                .enableAutoManage((FragmentActivity) context, inviteCallback)
+                .build();
+
+        // Check for App Invite invitations and launch deep-link activity if possible.
+        // Requires that an Activity is registered in AndroidManifest.xml to handle
+        // deep-link URLs.
+        AppInvite.AppInviteApi.getInvitation(googleApiClient, (Activity) context,
+                autoLaunchDeepLink)
+                .setResultCallback(
+                        new ResultCallback<AppInviteInvitationResult>() {
+                            @Override
+                            public void onResult(@NonNull AppInviteInvitationResult result) {
+                                if (!result.getStatus().isSuccess()) {
+                                    Log.w(TAG, "getInvitation:onResult:" + result.getStatus());
+                                    return;
+                                }
+
+                                // Extract information from the intent
+                                Intent intent = result.getInvitationIntent();
+                                String deepLink = AppInviteReferral.getDeepLink(intent);
+                                String invitationId = AppInviteReferral.getInvitationId(intent);
+
+                                Log.i(TAG, "getInvitation:onResult:" + result.getStatus() + ", "
+                                        + "intent: " + intent + ", deepLink: " + deepLink + ", " +
+                                        "invitationId: " + invitationId);
+
+                                // Because autoLaunchDeepLink = true we don't have to do
+                                // anything
+                                // here, but we could set that to false and manually choose
+                                // an Activity to launch to handle the deep link here.
+                                // ...
+
+                                // Only when opening from the invitation email
+                                if (Intent.ACTION_MAIN.equals(intent.getAction()) && deepLink !=
+                                        null) {
+                                    Uri deepLinkUri = Uri.parse(deepLink);
+                                    if (deepLink.startsWith(context.getString(R.string
+                                            .promotion_deep_link_promotion_code))) {
+                                        String promotionCode = deepLinkUri.getLastPathSegment();
+                                        inviteCallback.onPromotionCodeReceived(promotionCode);
+
+                                        Log.i(TAG, "getInvitation:promotionCode: " + promotionCode);
+
+                                    } else {
+                                        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                                                deepLinkUri);
+                                        context.startActivity(browserIntent);
+
+                                        Log.i(TAG, "getInvitation:deepLink started");
+                                    }
+                                }
+                            }
+                        });
     }
 }
